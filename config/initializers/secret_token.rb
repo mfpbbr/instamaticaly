2c3053d3eecf7db6a27fc871a0db822d47a882e1
Joe Chin <mail@joech.in>
2012-05-06 12:33:58 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instamaticaly::Application.config.secret_token = 'b2a9a35f03153403ade5e4b8adb9863a13116df6713bee1e5c098aa98c5bf6fb8ae6b9a3be63f8a66579fee80493996d554563f1941fb7a002cbe28423cb3832'
